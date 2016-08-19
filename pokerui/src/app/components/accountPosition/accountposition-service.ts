32d63658601028c8eeb506f3c8e23fc7cc8add2a
Robert Brem <brem_robert@hotmail.com>
2016-08-19 18:37:18 +0200

    0.1.0

--- pokerui/src/app/components/accountPosition/accountposition-service.ts
+++ pokerui/src/app/components/accountPosition/accountposition-service.ts
@@ -1,89 +1,76 @@
-import {Injectable} from '@angular/core';
-import {Http, Headers, RequestOptions} from '@angular/http';
-import 'rxjs/add/operator/map'
-import {Observable} from 'rxjs/Observable';
-import {Balance} from './../player/balance';
+import {Injectable} from "@angular/core";
+import {Http, Headers, RequestOptions} from "@angular/http";
+import "rxjs/add/operator/map";
+import {Observable} from "rxjs/Observable";
+import {Balance} from "./../player/balance";
 import {AccountPosition} from "./accountPosition";
 import {TimeEntry} from "./timeentry";
 import {HistoryEntry} from "./historyentry";
-import {KeycloakService} from '../../../keycloak';
+import {KeycloakService} from "../../../keycloak";
 
 @Injectable()
 export class AccountPositionService {
-  private playersUrl:string;
+  private playersCommandUrl: string;
+  private playersQueryUrl: string;
 
-  constructor(private http:Http, private kc:KeycloakService) {
-    let baseUrl:string = process.env.POKERTRACKER_URL;
-    if (baseUrl == undefined) {
-      baseUrl = 'http://localhost:8080/pokertracker/';
+  constructor(private http: Http, private kc: KeycloakService) {
+    let baseCommandUrl: string = process.env.POKERTRACKER_COMMAND_URL;
+    if (baseCommandUrl == undefined) {
+      baseCommandUrl = 'http://localhost:8282/pokertracker/';
     }
-    let api:string = 'resources/';
-    this.playersUrl = baseUrl + api + 'players/';
+    let baseQueryUrl: string = process.env.POKERTRACKER_QUERY_URL;
+    if (baseQueryUrl == undefined) {
+      baseQueryUrl = 'http://localhost:8080/pokertracker.query/';
+    }
+
+    let api: string = 'resources/';
+
+    this.playersCommandUrl = baseCommandUrl + api + 'players/';
+    this.playersQueryUrl = baseQueryUrl + api + 'players/';
   }
 
-  public create = (playerId:number, amount:number, currency:string):Promise<Observable<AccountPosition>> => {
+  public create = (playerId: number, amount: number, currency: string): Promise<Observable<AccountPosition>> => {
     var toAdd = JSON.stringify({playerId: playerId, amount: amount, currency: currency});
     return this.kc.getToken().then(token => {
       return this.http
-        .post(this.playersUrl + playerId + '/accountpositions/', toAdd, new RequestOptions({headers: this.getHeaders(token)}))
+        .post(this.playersCommandUrl + playerId + '/accountpositions/', toAdd, new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     });
   }
 
-  public getBalance = (playerId:number):Promise<Observable<Balance>> => {
-    // let key;
-    // for (key in KeycloakService.auth) {
-    //   console.log('key: ' + key);
-    //   console.log('value: ' + KeycloakService.auth[key]);
-    // }
-    // for (key in KeycloakService.auth.authz) {
-    //   console.log('key: ' + key);
-    //   console.log('value: ' + KeycloakService.auth.authz[key]);
-    // }
-    // for (key in KeycloakService.auth.authz['idTokenParsed']) {
-    //   console.log('key: ' + key);
-    //   console.log('value: ' + KeycloakService.auth.authz['idTokenParsed'][key]);
-    // }
-    // for (key in KeycloakService.auth.authz['tokenParsed']) {
-    //   console.log('key: ' + key);
-    //   console.log('value: ' + KeycloakService.auth.authz['tokenParsed'][key]);
-    // }
-    // for (key in KeycloakService.auth.authz['realmAccess']) {
-    //   console.log('key: ' + key);
-    //   console.log('value: ' + KeycloakService.auth.authz['realmAccess'][key]);
-    // }
+  public getBalance = (playerId: number): Promise<Observable<Balance>> => {
     return this.kc.getToken().then(token => {
       return this.http
-        .get(this.playersUrl + playerId + '/balance', new RequestOptions({headers: this.getHeaders(token)}))
+        .get(this.playersQueryUrl + playerId + '/balance', new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     });
   }
 
-  public getAccountPositions = (playerId:number):Promise<Observable<AccountPosition[]>> => {
+  public getAccountPositions = (playerId: number): Promise<Observable<AccountPosition[]>> => {
     return this.kc.getToken().then(token => {
       return this.http
-        .get(this.playersUrl + playerId + '/accountpositions/', new RequestOptions({headers: this.getHeaders(token)}))
+        .get(this.playersQueryUrl + playerId + '/accountpositions/', new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     });
   }
 
-  public getAccountHistoryForPlayer = (playerId:number):Promise<Observable<TimeEntry[]>> => {
+  public getAccountHistoryForPlayer = (playerId: number): Promise<Observable<TimeEntry[]>> => {
     return this.kc.getToken().then(token => {
       return this.http
-        .get(this.playersUrl + playerId + '/accounthistory/' + '?summedUp=true', new RequestOptions({headers: this.getHeaders(token)}))
+        .get(this.playersQueryUrl + playerId + '/accounthistory/' + '?summedUp=true', new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     });
   }
 
-  public getAccountHistory = (timeUnit:string, maxEntries:number):Promise<Observable<HistoryEntry[]>> => {
+  public getAccountHistory = (timeUnit: string, maxEntries: number): Promise<Observable<HistoryEntry[]>> => {
     return this.kc.getToken().then(token => {
       return this.http
-        .get(this.playersUrl + 'accounthistory/' + '?summedUp=true&timeUnit=' + timeUnit + '&maxEntries=' + maxEntries, new RequestOptions({headers: this.getHeaders(token)}))
+        .get(this.playersQueryUrl + 'accounthistory/' + '?summedUp=true&timeUnit=' + timeUnit + '&maxEntries=' + maxEntries, new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     });
   }
 
-  private getHeaders(token:string) {
+  private getHeaders(token: string) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
--- pokerui/src/app/components/player/player-service.ts
+++ pokerui/src/app/components/player/player-service.ts
@@ -1,64 +1,72 @@
-import {Injectable} from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
-import 'rxjs/add/operator/map'
-import {Observable} from 'rxjs/Observable';
-import {Player} from './player';
-import {KeycloakService} from '../../../keycloak';
+import {Injectable} from "@angular/core";
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
+import "rxjs/add/operator/map";
+import {Observable} from "rxjs/Observable";
+import {Player} from "./player";
+import {KeycloakService} from "../../../keycloak";
 
 @Injectable()
 export class PlayerService {
-  private playersUrl:string;
+  private playersCommandUrl: string;
+  private playersQueryUrl: string;
 
-  constructor(private http:Http, private kc:KeycloakService) {
-    let baseUrl:string = process.env.POKERTRACKER_URL;
-    if (baseUrl == undefined) {
-      baseUrl = 'http://localhost:8080/pokertracker/';
+  constructor(private http: Http, private kc: KeycloakService) {
+    let baseCommandUrl: string = process.env.POKERTRACKER_COMMAND_URL;
+    if (baseCommandUrl == undefined) {
+      baseCommandUrl = 'http://localhost:8282/pokertracker/';
     }
-    let api:string = 'resources/';
-    this.playersUrl = baseUrl + api + 'players/';
+    let baseQueryUrl: string = process.env.POKERTRACKER_QUERY_URL;
+    if (baseQueryUrl == undefined) {
+      baseQueryUrl = 'http://localhost:8080/pokertracker.query/';
+    }
+
+    let api: string = 'resources/';
+
+    this.playersCommandUrl = baseCommandUrl + api + 'players/';
+    this.playersQueryUrl = baseQueryUrl + api + 'players/';
   }
 
-  public getAll = ():Promise<Observable<Player[]>> => {
+  public getAll = (): Promise<Observable<Player[]>> => {
     return this.kc.getToken().then(token => {
       return this.http
-        .get(this.playersUrl, new RequestOptions({headers: this.getHeaders(token)}))
+        .get(this.playersQueryUrl, new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     })
   }
 
-  public find = (id:number):Promise<Observable<Player>> => {
+  public find = (id: number): Promise<Observable<Player>> => {
     return this.kc.getToken().then(token => {
       return this.http
-        .get(this.playersUrl + id, new RequestOptions({headers: this.getHeaders(token)}))
+        .get(this.playersQueryUrl + id, new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     });
   }
 
-  public create = (firstName:string, lastName:string):Promise<Observable<Player>> => {
+  public create = (firstName: string, lastName: string): Promise<Observable<Player>> => {
     var toAdd = JSON.stringify({firstName: firstName, lastName: lastName});
     return this.kc.getToken().then(token => {
       return this.http
-        .post(this.playersUrl, toAdd, new RequestOptions({headers: this.getHeaders(token)}))
+        .post(this.playersCommandUrl, toAdd, new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     });
   }
 
-  public update = (id:number, itemToUpdate:Player):Promise<Observable<Player>> => {
+  public update = (id: number, itemToUpdate: Player): Promise<Observable<Player>> => {
     return this.kc.getToken().then(token => {
       return this.http
-        .put(this.playersUrl + id, JSON.stringify(itemToUpdate), new RequestOptions({headers: this.getHeaders(token)}))
+        .put(this.playersCommandUrl + id, JSON.stringify(itemToUpdate), new RequestOptions({headers: this.getHeaders(token)}))
         .map(res => res.json());
     });
   }
 
-  public delete = (id:number):Promise<Observable<Response>> => {
+  public delete = (id: number): Promise<Observable<Response>> => {
     return this.kc.getToken().then(token => {
       return this.http
-        .delete(this.playersUrl + id, new RequestOptions({headers: this.getHeaders(token)}));
+        .delete(this.playersCommandUrl + id, new RequestOptions({headers: this.getHeaders(token)}));
     });
   }
 
-  private getHeaders(token:string) {
+  private getHeaders(token: string) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     headers.append('Accept', 'application/json');
